0417f828a447db58a4aebfb7746fe732a635efe0
BenjaminDelacombaz <bendelacombaz@gmail.com>
2020-06-19 16:54:06 +0200

    Create aut guard

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { map, first } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,5 +12,9 @@ export class AuthService {
 
   login(email: string, password: string): Promise<firebase.auth.UserCredential> {
     return this.angularFireAuth.signInWithEmailAndPassword(email, password)
+  }
+
+  isAuthenticated(): Observable<boolean> {
+    return this.angularFireAuth.user.pipe(map(user => user != null && user.uid != null), first())
   }
 }

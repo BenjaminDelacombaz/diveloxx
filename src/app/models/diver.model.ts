6dba664fbee770691ccaadc01a080a54d31e2238
BenjaminDelacombaz <bendelacombaz@gmail.com>
2020-06-19 15:38:20 +0200

    Move diver service and add getDiver function

--- src/app/models/diver.model.ts
+++ src/app/models/diver.model.ts
@@ -1,6 +1,19 @@
-class Diver {
+export interface DiverInterface {
+    name: string
+    email: string
+    phone: string
+    birthdate: firebase.firestore.Timestamp
+}
+export class Diver implements DiverInterface {
     public name: string
     public email: string
     public phone: string
     public birthdate: firebase.firestore.Timestamp
+
+    constructor(diver: DiverInterface) {
+        this.name = diver.name
+        this.email = diver.email
+        this.phone = diver.phone
+        this.birthdate = diver.birthdate
+    }
 }
---
+++ src/app/services/diver/diver.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { map } from 'rxjs/operators'
+import { Diver, DiverInterface } from '../../models/diver.model'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DiverService {
+
+  private docPath: string = '/divers'
+
+  constructor(private angularFireStore: AngularFirestore) { }
+
+  getDiver(uid: string) {
+    return this.angularFireStore
+      .doc<Diver>(`${this.docPath}/${uid}`)
+      .valueChanges()
+      .pipe(map((diver: DiverInterface) => new Diver(diver)))
+  }
+}

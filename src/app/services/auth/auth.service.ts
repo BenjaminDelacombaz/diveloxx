568c6cab86fc7c019550245d308ad5c81c3943fc
BenjaminDelacombaz <bendelacombaz@gmail.com>
2020-06-19 15:09:07 +0200

    Create login function in aut service

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private angularFireAuth: AngularFireAuth) { }
+
+  login(email: string, password: string): Promise<firebase.auth.UserCredential> {
+    return this.angularFireAuth.signInWithEmailAndPassword(email, password)
+  }
 }

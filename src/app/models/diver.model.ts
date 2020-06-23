8cc604f59673a02857b386fb6d07fda48f7f927e
BenjaminDelacombaz <bendelacombaz@gmail.com>
2020-06-23 17:11:09 +0200

    Change name by firstname and lastname

--- src/app/models/diver.model.ts
+++ src/app/models/diver.model.ts
@@ -1,19 +1,26 @@
 export interface DiverInterface {
-    name: string
+    firstname: string
+    lastname: string
     email: string
     phone: string
     birthdate: firebase.firestore.Timestamp
 }
 export class Diver implements DiverInterface {
-    public name: string
+    public firstname: string
+    public lastname: string
     public email: string
     public phone: string
     public birthdate: firebase.firestore.Timestamp
 
     constructor(diver: DiverInterface) {
-        this.name = diver.name
+        this.firstname = diver.firstname
+        this.lastname = diver.lastname
         this.email = diver.email
         this.phone = diver.phone
         this.birthdate = diver.birthdate
+    }
+
+    toString(): string {
+        return `${this.firstname} ${this.lastname}`
     }
 }

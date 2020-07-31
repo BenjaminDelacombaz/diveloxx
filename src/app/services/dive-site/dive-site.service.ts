caba08c5880419658326f9fe719b5c22d21040b6
BenjaminDelacombaz <bendelacombaz@gmail.com>
2020-07-31 11:41:41 +0200

    Add create and get dive site

--- src/app/services/dive-site/dive-site.service.ts
+++ src/app/services/dive-site/dive-site.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { DiveSite, DiveSiteInterface } from 'src/app/models/dive-site.model';
-import { map } from 'rxjs/operators';
+import { map, first } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+import { DiverService } from '../diver/diver.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,7 @@ export class DiveSiteService {
 
   constructor(
     private angularFirestore: AngularFirestore,
+    private diverService: DiverService,
   ) { }
 
   getDiveSites(): Observable<DiveSite[]> {
@@ -22,5 +24,26 @@ export class DiveSiteService {
       .pipe(
         map((diveSitesI: DiveSiteInterface[]) => diveSitesI.map(diveSiteI => new DiveSite(diveSiteI)))
       )
+  }
+
+  getDiveSite(docId: string) {
+    return this.angularFirestore
+      .doc<DiveSiteInterface>(`${this.docPath}/${docId}`)
+      .valueChanges()
+      .pipe(
+        map((diveSite: DiveSiteInterface) => {
+          return diveSite ? new DiveSite(diveSite) : null
+        })
+      )
+  }
+
+  async create(diveSiteI: DiveSiteInterface): Promise<Observable<DiveSite>> {
+    let docId: string = this.angularFirestore.createId()
+    // Set id
+    diveSiteI.id = docId
+    // Set owner id
+    diveSiteI.owner_id = this.diverService.currentDiver.id
+    await this.angularFirestore.doc<DiveSiteInterface>(`${this.docPath}/${docId}`).set(diveSiteI)
+    return this.getDiveSite(docId).pipe(first())
   }
 }

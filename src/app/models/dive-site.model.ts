b06812036d6535595243743741756cec4df28a4a
BenjaminDelacombaz <bendelacombaz@gmail.com>
2020-07-31 10:54:13 +0200

    Create dive site model

---
+++ src/app/models/dive-site.model.ts
@@ -0,0 +1,31 @@
+export interface DiveSiteInterface {
+    id: string
+    description: string
+    difficulty: number
+    location: firebase.firestore.GeoPoint
+    name: string
+    water_type: string
+    owner_id: string
+}
+export class DiveSite implements DiveSiteInterface {
+    public id: string
+    public description: string
+    public difficulty: number
+    public location: firebase.firestore.GeoPoint
+    public name: string
+    public water_type: string
+    public owner_id: string
+
+    constructor(diveSite: DiveSiteInterface) {
+        this.id = diveSite.id
+        this.description = diveSite.description
+        this.difficulty = diveSite.difficulty
+        this.location = diveSite.location
+        this.name = diveSite.name
+        this.owner_id = diveSite.owner_id
+    }
+
+    toString(): string {
+        return this.name
+    }
+}

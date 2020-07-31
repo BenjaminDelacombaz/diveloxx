7081f5aca85241e981ae5ccdfb70355a494fb24d
BenjaminDelacombaz <bendelacombaz@gmail.com>
2020-07-31 11:10:21 +0200

    Add getDiveSites function

--- src/app/services/dive-site/dive-site.service.ts
+++ src/app/services/dive-site/dive-site.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { DiveSite, DiveSiteInterface } from 'src/app/models/dive-site.model';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DiveSiteService {
 
-  constructor() { }
+  private docPath: string = '/dive-sites'
+
+  constructor(
+    private angularFirestore: AngularFirestore,
+  ) { }
+
+  getDiveSites(): Observable<DiveSite[]> {
+    return this.angularFirestore
+      .collection<DiveSiteInterface>(this.docPath)
+      .valueChanges()
+      .pipe(
+        map((diveSitesI: DiveSiteInterface[]) => diveSitesI.map(diveSiteI => new DiveSite(diveSiteI)))
+      )
+  }
 }

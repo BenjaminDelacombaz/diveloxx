549a70abf11ce6199e1d87d18902cc057f5193c9
BenjaminDelacombaz <bendelacombaz@gmail.com>
2020-08-03 14:27:03 +0200

    Add map and functions for dive site selection

--- src/app/components/dive-site-select-location-map-modal/dive-site-select-location-map-modal.page.ts
+++ src/app/components/dive-site-select-location-map-modal/dive-site-select-location-map-modal.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, OnInit, Input } from '@angular/core';
+import { ModalController, ToastController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-dive-site-select-location-map-modal',
@@ -8,13 +9,38 @@ import { ModalController } from '@ionic/angular';
 })
 export class DiveSiteSelectLocationMapModalPage implements OnInit {
 
-  constructor(private modalController: ModalController) { }
+  @Input() diveSiteLocation: google.maps.LatLng
+
+  constructor(
+    private modalController: ModalController,
+    private toastController: ToastController,
+    private translate: TranslateService,
+  ) { }
 
   ngOnInit() {
   }
 
   dismissModal() {
     this.modalController.dismiss()
+  }
+
+  createMarker(event: google.maps.MouseEvent) {
+    this.diveSiteLocation = event.latLng
+  }
+
+  async save() {
+    if (this.diveSiteLocation) {
+      this.modalController.dismiss({
+        diveSiteLocation: this.diveSiteLocation,
+      })
+    } else {
+      // Display error toast
+      ; (await this.toastController.create({
+        message: this.translate.instant('diveSiteSelectLocationMapModalComponent.noLocation'),
+        duration: 5000,
+        color: 'danger',
+      })).present()
+    }
   }
 
 }

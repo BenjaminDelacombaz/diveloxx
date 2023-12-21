5336e4a7bf48c7296bf7e1b594afeaca400a3b02
BenjaminDelacombaz <31914427+BenjaminDelacombaz@users.noreply.github.com>
2023-12-21 15:32:08 +0100

    test

--- src/altis_incident/__manifest__.py
+++ src/altis_incident/__manifest__.py
@@ -10,7 +10,7 @@
     """,
 
     'author': "Odoo PS",
-    'website': "http://www.odoo.com",
+    'website': "http://www.odoo.com",tutu
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
